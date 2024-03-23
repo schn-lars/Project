124638331356088748e557afcf1421572f83cc4b
schn-lars <lars.schneider@stud.unibas.ch>
2024-03-23 16:07:27 +0100

    Add commandline parser to shell. Now extracts commands and their arguments. [quit] is the only command defined so far.

--- src/davis.c
+++ src/davis.c
@@ -1,24 +1,31 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "errno.h"
 
 /*
  * This is the main access point for our custom shell.
  */
 
-#define MAX_INPUT_BUFFER    128
+#define MAX_INPUT_BUFFER    128 // Including zero byte at the end
+#define MAX_COMMAND_LENGTH  10
 #define MAX_INPUT_COUNT     8
 
 char input[MAX_INPUT_BUFFER];
+char command[MAX_COMMAND_LENGTH];
 pid_t pid; // Process number
-char *argv[8];
+char *arguments[8];
+int input_length;
+int shell_running;
 
 /*
  * Declaring functions for our shell.
  */
 void davis();
 void get_input();
-void exec_input();
+void parse_input_into_commands();
+void exec_command();
+
 
 int main()
 {
@@ -32,10 +39,11 @@ void davis()
 {
     // TODO vielleicht eine grafische Fläche zu Beginn
     printf("Hey, I'm DAVIS. How may I assist You?\n");
-    while (1) {
+    shell_running = 1;
+    while (shell_running) {
         get_input();
-        exec_input();
-        if(!strcmp("quit", input)) break;
+        parse_input_into_commands();
+        exec_command();
     }
 }
 
@@ -46,7 +54,45 @@ void get_input()
 {
     printf("[DAVIS]\t");
     fgets(input, MAX_INPUT_BUFFER, stdin);
-    int input_length = strlen(input);
+    input_length = strlen(input);
     if ((input_length > 0) && (input[input_length - 1] == '\n'))
         input[input_length - 1] = '\0'; // Terminate string with zero-byte
+}
+
+/*
+ * Translating the given input in syntactically correct parameters.
+ */
+void parse_input_into_commands() {
+    int word_counter = 0;
+    char *token = strtok(input, " ");
+    if (token != NULL) {
+        // The first token is the command itself.
+        strncpy(command, token, MAX_COMMAND_LENGTH - 1);
+        command[MAX_COMMAND_LENGTH - 1] = '\0';
+
+        // Cut arguments into pieces. If there are more arguments than 10, they would simply be ignored.
+        while ((token = strtok(NULL, " ")) != NULL && word_counter < MAX_INPUT_COUNT) {
+            arguments[word_counter] = token;
+            word_counter++;
+        }
+    }
+}
+
+/*
+ * Executes the command, previously saved in command originating from input.
+ */
+void exec_command() {
+    printf("Command: %s\n", command);
+
+    if (strcmp(command, "quit") == 0) {
+        shell_running = 0;
+    } else {
+        printf("Unknown command: %s\n", command);
+    }
+
+    // Set arguments and command to null after usage.
+    memset(command, 0, sizeof(command));
+    for (int i = 0; i < MAX_INPUT_COUNT; i++) {
+        arguments[i] = NULL;
+    }
 }
