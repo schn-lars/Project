3508796326829a1438d496158b47658bd711b087
mircofrco <m.franco@stud.unibas.ch>
2024-04-04 20:57:38 +0200

    Adjust header of plot.h to work like the other commands

--- src/commands/plot.h
+++ src/commands/plot.h
@@ -1,7 +1,13 @@
 /*
- *  plot [options] -> plots something like idk
+ *  plot [options] -> plots a given input with the GNUplot-library and shows a graph
  */
 #include <stdio.h>
 #include <stdlib.h>
 
-int plot_file(char *PATH);
+#ifndef PLOT_H
+#define PLOT_H
+#include "../davis.h"
+
+void plot();
+
+#endif
