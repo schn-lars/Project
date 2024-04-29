3bb21c6727d0de358884d254bf805cf6b6dc1f5a
mircofrco <m.franco@stud.unibas.ch>
2024-04-29 21:20:37 +0200

    Add checkFile to plot.c to check if the given input is a file or a direct function

--- src/commands/plot.c
+++ src/commands/plot.c
@@ -1,23 +1,87 @@
 #include "plot.h"
 
+
 int plot(char **args) {
     if (args[1] == NULL) {
         printf("Missing data.\n");
         return FAILURE;
     }
-    const char* filename = args[1];
-    char command[100];
-    FILE *gnuplotPipe = popen("gnuplot -persist", "w");
-    if (!gnuplotPipe) {
-        printf("Error opening Gnuplot.\n");
-        return FAILURE;
+    const char* function = args[1];
+
+    printf("Input: %s \n", function);
+
+
+    // Check if file exists or not
+    if (checkFile(function))
+    {
+        printf("Access: File exists at path '%s'\n", function);
+        char command[100];
+        FILE *gnuplotPipe = popen("gnuplot -persist", "w");
+        if (!gnuplotPipe) {
+            printf("Error opening Gnuplot.\n");
+            return FAILURE;
+        }
+        printf("IF1 --- plot '%s' with lines\n", function);
+        sprintf(command, "plot '%s' with lines\n", function);
+        fprintf(gnuplotPipe, "%s", command);
+        fflush(gnuplotPipe);
+        fprintf(gnuplotPipe, "exit\n");
+
+        pclose(gnuplotPipe);
+        return SUCCESS;
+    }
+    else if (strstr(function, "'") != NULL || strchr(function, '"') != NULL)
+    {
+        printf("Access: File exists at path '%s'\n", function);
+        char command[100];
+        FILE *gnuplotPipe = popen("gnuplot -persist", "w");
+        if (!gnuplotPipe) {
+            printf("Error opening Gnuplot.\n");
+            return FAILURE;
+        }
+        printf("IF2 --- plot %s with lines\n", function);
+        sprintf(command, "plot %s with lines\n", function);
+        fprintf(gnuplotPipe, "%s", command);
+        fflush(gnuplotPipe);
+        fprintf(gnuplotPipe, "exit\n");
+
+        pclose(gnuplotPipe);
+        return SUCCESS;
+    }
+    else if (checkFile(function) == 0)
+    {
+        printf("Access: File does not exists at path '%s'\n", function); // direct function not a file
+        char command[100];
+        FILE *gnuplotPipe = popen("gnuplot -persist", "w");
+        if (!gnuplotPipe) {
+            printf("Error opening Gnuplot.\n");
+            return FAILURE;
+        }
+        printf("IF3 --- plot %s with lines\n", function);
+        sprintf(command, "plot %s with lines\n", function);
+        fprintf(gnuplotPipe, "%s", command);
+        fflush(gnuplotPipe);
+        fprintf(gnuplotPipe, "exit\n");
+
+        pclose(gnuplotPipe);
+        return SUCCESS;
     }
 
-    sprintf(command, "plot '%s' with lines\n", filename);
-    fprintf(gnuplotPipe, "%s", command);
-    fflush(gnuplotPipe);
-    fprintf(gnuplotPipe, "exit\n");
+    return FAILURE;
+}
 
-    pclose(gnuplotPipe);
-    return SUCCESS;
+
+
+/**
+ * Function to check whether a file exists or not using
+ * access() function. It returns 1 if file exists at
+ * given path otherwise returns 0.
+ */
+int checkFile(const char *path)
+{
+    // Check for file existence
+    if (access(path, F_OK) == -1)
+        return 0;
+
+    return 1;
 }
--- src/commands/plot.h
+++ src/commands/plot.h
@@ -1,13 +1,20 @@
 /*
- *  plot [options] -> plots a given input with the GNUplot-library and shows a graph
+ *  plot <~/PATH/TO/DATA> -> plots a given input with the GNUplot-library and shows a graph
+ *  the settings of a graph are per default:
+ *      - with lines
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #ifndef PLOT_H
 #define PLOT_H
 #include "../davis.h"
 
+
 int plot(char **args);
+int checkFile(const char *path);
 
 #endif
