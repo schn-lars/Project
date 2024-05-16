bde789ac55ccac580146b273be27b1454f03336e
mircofrco <m.franco@stud.unibas.ch>
2024-05-16 09:45:39 +0200

    Add header with correct flags and arguments

--- src/commands/plot.h
+++ src/commands/plot.h
@@ -1,8 +1,28 @@
 /*
- *  plot <~/PATH/TO/DATA> -> plots a given input with the GNUplot-library and shows a graph
+ *  plot <~/PATH/TO/DATA>  {optional flags} {optional arguments} -> plots a given input with the GNUplot-library and shows a graph
+ *  Possible flags are:
+ *      -b → removes top and right borders
+ *      -g → removes grid
+ *      -e → adds errorbars (only possible with three columns in data)
+ *      -p → graph with points
+ *      -l -> graph with lines
+ *      -l & -p → graph with lines and points
+ *      -d → disables darkmode
+ *      -s → saves graph as png directly
+ *  Possible arguments (most arguments work with their short form SF or alternativ form AF):
+ *      title:<title> → sets the title of the graph (SF tit)
+ *      xlabel:<xlabel> → sets the horizontal xlabel (SF xl)
+ *      ylabel:<ylabel> → sets the vertical ylabel (SF yl)
+ *      color:#ffffff → changes color of graph lines/points to hexa value OR (AF colour)
+ *      color:red → changes color to corresponding color name (see GNUplot list of colors) (AF colour)
+ *      xrange:[<start>:<end>] → limits range of x-values to given input. For example: xr:[-2:2] (SF xr)
+ *      yrange:[<start>:<end>] → limits range of y-values to given input. For example: yr:[-2:2] (SF yr)
+ *      legend:<position> → moves legend to different corner (AF box, key)
+ *          Inputs are: northeast OR ne / northwest OR nw / southeast OR se / southwest OR sw / northcenter OR nc / southcenter OR sc
  *  the settings of a graph are per default:
  *      - with lines
- *
+ *      - DAVIS graph colors
+ *      - darkmode
  */
 #include <stdio.h>
 #include <stdlib.h>
