dff75473ae0871a3b20ca480592b1f6f7d890312
schn-lars <lars.schneider@stud.unibas.ch>
2024-05-18 16:25:46 +0200

    Add echo with redirect option into files.

--- src/commands/echo.c
+++ src/commands/echo.c
@@ -9,38 +9,63 @@ int echo(char **args) {
         warn("Missing data. Usage echo [-FLAGS] <data>");
         return FAILURE;
     }
-    set_background(args[1]);
-    set_style(args[1]);
-    set_color(args[1]);
-    if (color != NULL) printf("%s", color);
-    if (style != NULL) printf("%s", style);
-    if (background != NULL) printf("%s", background);
-    for (int i = 1; i < MAX_INPUT_COUNT && args[i] != NULL && args[i][0] != '\0'; i++) {
-        if (args[i][0] != '-') {
-            printf("%s", args[i]);
-            if (args[i + 1] != NULL) printf(" ");
+    int redirect = check_for_redirect(args);
+    if (redirect != -1) {
+        printf("Redirect %d", redirect);
+        for (int j = redirect + 1; j < MAX_INPUT_COUNT && args[j] != NULL; j++) {
+            printf("Creating file %s\n", args[j]);
+            FILE *file = fopen(args[j], "a");
+            if (file == NULL) {
+                warn("ERROR: Could not create file.");
+                return FAILURE;
+            } else {
+                for (int k = 1; k < redirect; k++) {
+                    if (args[k][0] != '-') {
+                        fprintf(file, "%s", args[k]);
+                        if (k < redirect - 1) {
+                            fprintf(file, " ");
+                        }
+                    }
+                }
+                fprintf(file, "\n");
+                fclose(file);
+            }
         }
+        return SUCCESS;
+    } else {
+        set_background(args[1]);
+        set_style(args[1]);
+        set_color(args[1]);
+        if (color != NULL) printf("%s", color);
+        if (style != NULL) printf("%s", style);
+        if (background != NULL) printf("%s", background);
+        for (int i = 1; i < MAX_INPUT_COUNT && args[i] != NULL && args[i][0] != '\0'; i++) {
+            if (args[i][0] != '-') {
+                printf("%s", args[i]);
+                if (args[i + 1] != NULL) printf(" ");
+            }
+        }
+        if (color != NULL || style != NULL || background != NULL) printf("%s", RESET);
+        printf("\n");
+        free(background);
+        free(color);
+        free(style);
+        return SUCCESS;
     }
-    if (color != NULL || style != NULL || background != NULL) printf("%s", RESET);
-    free(background);
-    free(color);
-    free(style);
-    return SUCCESS;
 }
 
 void set_style(char *args)
 {
-    style = calloc(15, sizeof(char));
+    style = calloc(40, sizeof(char));
     if (args != NULL && args[0] == '-') {
         if (strstr(args, "F") != NULL) {
-            strcpy(style, STYLE_BOLD);
-            return;
-        } else if (strstr(args, "I") != NULL) {
-            strcpy(style, STYLE_ITALIC);
-            return;
-        } else if (strstr(args, "U") != NULL) {
-            strcpy(style, STYLE_UNDERLINE);
-            return;
+            strcat(style, STYLE_BOLD);
+        }
+        if (strstr(args, "I") != NULL) {
+            strcat(style, STYLE_ITALIC);
+        }
+        if (strstr(args, "U") != NULL) {
+            strcat(style, STYLE_UNDERLINE);
         }
     }
 }
@@ -107,4 +132,14 @@ void set_background(char *args)
             return;
         }
     }
+}
+
+int check_for_redirect(char **args)
+{
+    for (int i = 1; i < MAX_INPUT_COUNT && args[i] != NULL; i++) {
+        if (strcmp(args[i], ">>") == 0) {
+            return i;
+        }
+    }
+    return -1;
 }
--- src/commands/echo.h
+++ src/commands/echo.h
@@ -1,11 +1,21 @@
 /*
- * echo <text> {optional flags} -> gives back the given text and prints it out
- * Possible flags are:
- *      -n => prints each word on a new line
- *      -r => prints each word in a different color
- *      -b => prints the text in bold
- *      -c => prints the text in cursive
- *      -u => prints the text with an underline
+ *  echo {optional flags} <text> -> gives back the given text and prints it out
+ *  Possible flags are:
+ *      Colors (lowercase = font & captial = background):
+ *          -b / B => Blue
+ *          -d / D => Black
+ *          -r / R => Red
+ *          -g / G => Green
+ *          -y / Y => Yellow
+ *          -m / M => Magenta
+ *          -w / W => White
+ *          -c / C => Cyan
+ *       Styles:
+ *          -F => Bold
+ *          -I => Cursive
+ *          -U => Underlined
+ *
+ *  echo <text> >> <files ...> -> prints text into specified files
  */
 #ifndef ECHO_H
 #define ECHO_H
@@ -45,5 +55,6 @@ int echo(char** args);
 void set_style(char *args);
 void set_color(char *args);
 void set_background(char *args);
+int check_for_redirect(char **args);
 
 #endif
